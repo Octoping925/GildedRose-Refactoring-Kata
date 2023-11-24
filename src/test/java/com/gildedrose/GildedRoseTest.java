b8c7a1242f749b356a926f6b112f5c967f074654
Octoping <mooner925@gmail.com>
2023-11-24 14:43:31 +0900

    test: BackstagePasses 테스트 작성

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -95,6 +95,7 @@ class GildedRoseTest {
     @DisplayName("Sulfuras는")
     class SulfurasTest {
         private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
         @Test
         @DisplayName("시간이 지나도 quality가 변하지 않는다")
         void qualityNotReduceTest() {
@@ -109,4 +110,71 @@ class GildedRoseTest {
             assertThat(sulfuras.quality).isEqualTo(80);
         }
     }
+
+    @Nested
+    @DisplayName("Backstage passes는")
+    class BackstagePassesTest {
+        private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
+        @Test
+        @DisplayName("SellIn 값이 10일 전까지는 1씩 증가한다")
+        void sellInBefore10Test() {
+            // given
+            Item backStagePasses = new Item(BACKSTAGE_PASSES, 11, 1);
+
+            // when
+            GildedRose app = new GildedRose(new Item[]{backStagePasses});
+            app.updateQuality();
+
+            // then
+            assertThat(backStagePasses.sellIn).isEqualTo(10);
+            assertThat(backStagePasses.quality).isEqualTo(2);
+        }
+
+        @Test
+        @DisplayName("SellIn 값이 10일부터 5일 전까지는 2씩 증가한다")
+        void sellInBefore5Test() {
+            // given
+            Item backStagePasses = new Item(BACKSTAGE_PASSES, 10, 1);
+
+            // when
+            GildedRose app = new GildedRose(new Item[]{backStagePasses});
+            app.updateQuality();
+
+            // then
+            assertThat(backStagePasses.sellIn).isEqualTo(9);
+            assertThat(backStagePasses.quality).isEqualTo(3);
+        }
+
+        @Test
+        @DisplayName("SellIn 값이 5일부터 0일 전까지는 3씩 증가한다")
+        void sellInBeforeStartTest() {
+            // given
+            Item backStagePasses = new Item(BACKSTAGE_PASSES, 5, 1);
+
+            // when
+            GildedRose app = new GildedRose(new Item[]{backStagePasses});
+            app.updateQuality();
+
+            // then
+            assertThat(backStagePasses.sellIn).isEqualTo(4);
+            assertThat(backStagePasses.quality).isEqualTo(4);
+        }
+
+        @Test
+        @DisplayName("SellIn 값이 0일이 되면 quality는 0이 된다")
+        void sellInEndTest() {
+            // given
+            Item backStagePasses = new Item(BACKSTAGE_PASSES, 0, 50);
+
+            // when
+            GildedRose app = new GildedRose(new Item[]{backStagePasses});
+            app.updateQuality();
+
+            // then
+            assertThat(backStagePasses.sellIn).isEqualTo(-1);
+            assertThat(backStagePasses.quality).isZero();
+        }
+    }
+
 }
