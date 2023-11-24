58b2082ff4d6c5b0ccf85f77f35663281d414689
Octoping <mooner925@gmail.com>
2023-11-24 15:50:28 +0900

    refactor: 매직 넘버 제거

--- src/main/java/com/gildedrose/qualityupdater/AgedBrieQualityUpdater.java
+++ src/main/java/com/gildedrose/qualityupdater/AgedBrieQualityUpdater.java
@@ -3,12 +3,13 @@ package com.gildedrose.qualityupdater;
 import com.gildedrose.Item;
 
 public class AgedBrieQualityUpdater implements QualityUpdater {
+    private static final int MAX_QUALITY = 50;
+
     @Override
     public void updateQuality(Item item) {
         reduceSellIn(item);
         increaseQuality(item);
     }
-
 
     private void reduceSellIn(Item item) {
         item.sellIn = item.sellIn - 1;
@@ -19,8 +20,8 @@ public class AgedBrieQualityUpdater implements QualityUpdater {
     }
 
     private int calculateIncreaseQualityAmount(Item item) {
-        if (item.quality >= 50) {
-            return  0;
+        if (item.quality >= MAX_QUALITY) {
+            return 0;
         }
 
         return 1;
